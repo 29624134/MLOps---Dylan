933d8fffaa12376dd81e57f599c87dde0755e58b
DylanOpp <29624134@sun.ac.za>
2026-01-27 13:59:53 +0200

    General Update

--- Old/main_manual.py
+++ Old/main_manual.py
@@ -2,5 +2,5 @@
 from orchestrator import WorkflowExecutor
 
 if __name__ == "__main__":
-    executor = WorkflowExecutor("config/workflow.yaml")
+    executor = WorkflowExecutor("../config/workflow.yaml")
     executor.start_workflow()
